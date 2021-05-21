22e25c395cd35a2e8c5d7af3aea9a89a2ed70241
marlond18 <marlon.dumalis@yahoo.com>
2021-05-21 13:17:26 +0800

    Excluded Core from sln

--- src/EMDD.KtEquatable.Core/Attributes/ReferenceEqualityAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EMDD.KtEquatable.Core.Attributes
-{
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ReferenceEqualityAttribute : Attribute
-    {
-    }
-}
