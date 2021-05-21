b81840e1bb8795634fe78b7ddd1b44325fccee41
marlond18 <marlon.dumalis@yahoo.com>
2021-05-21 13:15:34 +0800

    Added SummaryString Class

---
+++ src/EMDD.KtEquatable/Core/Summary/SummaryParam.cs
@@ -0,0 +1,14 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace EMDD.KtEquatable.Core
+{
+    public class SummaryParam
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/EMDD.KtEquatable/Core/Summary/SummaryString.cs
@@ -0,0 +1,15 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace EMDD.KtEquatable.Core
+{
+    public class SummaryString
+    {
+        public List<(string content, bool newLine)> Contents { get; set; } = new List<(string content, bool newLine)>();
+        public List<SummaryParam> Params { get; set; } = new List<SummaryParam>();
+
+        public string Returns { get; set; }
+    }
+}
