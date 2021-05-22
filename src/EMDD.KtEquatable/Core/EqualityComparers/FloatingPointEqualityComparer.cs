e21869603877b12d211799404460e2b058326fb0
marlond18 <marlon.dumalis@yahoo.com>
2021-05-22 15:09:51 +0800

    revert the edit back

--- src/EMDD.KtEquatable/Core/EqualityComparers/FloatingPointEqualityComparer.cs
+++ src/EMDD.KtEquatable/Core/EqualityComparers/FloatingPointEqualityComparer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace EMDD.KtEquatable.Core.EqualityComparers
 {
-    public class FloatingPointEqualityComparer : IEqualityComparer<double?>
+    public class FloatingPointEqualityComparer : IEqualityComparer<double>
     {
         private readonly int precision;
 
@@ -12,16 +12,14 @@ namespace EMDD.KtEquatable.Core.EqualityComparers
             this.precision = precision;
         }
 
-        public bool Equals(double? x, double? y)
+        public bool Equals(double x, double y)
         {
-            if (ReferenceEquals(x, y)) return true;
-            if (x is null || y is null) return false;
-            return x.Value.NearEquals(y.Value, precision);
+            return x.NearEquals(y, precision);
         }
 
-        public int GetHashCode(double? obj)
+        public int GetHashCode(double obj)
         {
-            return obj is null? obj.GetHashCode(): obj.Value.GetDoubleHashCode(precision);
+            return obj.GetDoubleHashCode(precision);
         }
     }
 }
