0789b0df9e403cc0f7c8a66876dea14f640a3a71
marlond18 <marlon.dumalis@yahoo.com>
2021-05-27 23:07:03 +0800

    removed package test

--- test/NugetPackageTester/Class1.cs
+++ test/NugetPackageTester/Class1.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using EMDD.KtEquatable.Core.Attributes;
 namespace NugetPackageTester
 {
-    [Equatable]
     public partial class Class1
     {
-        [ReferenceEquality]
         public string? MyProperty { get; set; }
     }
 }
